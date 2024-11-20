5963c883443ae88f10ce4a952ac7d160f698e204
Recarrdo <86822815+Recarrdo@users.noreply.github.com>
2024-11-20 13:30:20 +0900

    site edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
+  site: 'https://Recarrdo.github.io/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
